0d3c0b50492245e10a0e520d839e8fa83a36fb61
Riccardo Carlesso <ricc+chezmoi@google.com>
2025-11-24 12:06:11 +0100

    saving ids

--- bin/deploy-to-netlify-images.sh
+++ bin/deploy-to-netlify-images.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+
+SITE_NAME="onramp-images"
+SITE_ID="7f419d2e-4589-46da-b48e-e7efba1751e1" # onramp-images site ID
+
 # This script deploys the onramp-images directory to the 'onramp-images' Netlify site.
-# The first time you run this, you will be prompted to create the new site.
 
 # Get the directory of this script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -10,5 +13,15 @@ PROJECT_ROOT="$SCRIPT_DIR/.."
 
 cd "$PROJECT_ROOT"
 
+# Move the state.json file to a temporary location
+if [ -f ".netlify/state.json" ]; then
+    mv .netlify/state.json .netlify/state.json.tmp
+fi
+
 echo "Deploying 'onramp-images' directory to site 'onramp-images' on Netlify..."
-netlify deploy --dir=onramp-images --prod --site onramp-images
+netlify deploy --dir=onramp-images --prod --site 7f419d2e-4589-46da-b48e-e7efba1751e1
+
+# Move the state.json file back
+if [ -f ".netlify/state.json.tmp" ]; then
+    mv .netlify/state.json.tmp .netlify/state.json
+fi
--- bin/deploy-to-netlify.sh
+++ bin/deploy-to-netlify.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+SITE_NAME="onramp-new"
+SITE_ID="5fe90bd5-5158-422e-a162-cef927f0e8ca" # onramp-new site ID
+
 # This script deploys the onramp-new directory to the 'onramp-new' Netlify site.
 
 # Get the directory of this script
@@ -9,5 +12,15 @@ PROJECT_ROOT="$SCRIPT_DIR/.."
 
 cd "$PROJECT_ROOT"
 
+# Move the state.json file to a temporary location
+if [ -f ".netlify/state.json" ]; then
+    mv .netlify/state.json .netlify/state.json.tmp
+fi
+
 echo "Deploying 'onramp-new' directory to site 'onramp-new' on Netlify..."
 netlify deploy --dir=onramp-new --prod --site onramp-new
+
+# Move the state.json file back
+if [ -f ".netlify/state.json.tmp" ]; then
+    mv .netlify/state.json.tmp .netlify/state.json
+fi
